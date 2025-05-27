b1b1cdef76be77e1ec7ae5ff788e67764ddf424a
waqar <codedevelopmentenv@gmail.com>
2025-05-28 00:56:10 +0200

    Adding the bug

--- Application-Code/frontend/src/services/vulnerable.js
+++ Application-Code/frontend/src/services/vulnerable.js
@@ -1,14 +1,14 @@
-// vulnerable.js
+vulnerable.js
 
-// function runUserCode() {
-//     const userCode = location.hash.substring(1);
-//     eval(userCode); // 🚩 S1523: Use of eval
-//   }
+function runUserCode() {
+    const userCode = location.hash.substring(1);
+    eval(userCode); // 🚩 S1523: Use of eval
+  }
   
-//   const password = "123456"; // 🚩 S2068: Hardcoded password
+  const password = "123456"; // 🚩 S2068: Hardcoded password
   
-//   fetch("http://insecure-api.com"); // 🚩 S4423: Insecure HTTP
+  fetch("http://insecure-api.com"); // 🚩 S4423: Insecure HTTP
   
-//   const userId = req.query.id;
-//   const sql = "SELECT * FROM users WHERE id = " + userId; // 🚩 S3649: SQL injection
+  const userId = req.query.id;
+  const sql = "SELECT * FROM users WHERE id = " + userId; // 🚩 S3649: SQL injection
   
